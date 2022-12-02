49e08d45a329a9a517eb1fc5bc8f95f9a50fd5a8
hojungpark-sfu <tacopak21@gmail.com>
2022-12-01 17:06:26 -0800

    create checkURL function

--- fastURLChecker.go
+++ fastURLChecker.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main(){
 	urls := []string{
 		"https://github.com/",
@@ -8,4 +13,13 @@ func main(){
 		"https://airbnb.com",
 		"https://indeed.com",
 	}
+
+	for _, url := range urls{
+		checkURL(url)
+	}
+}
+
+func checkURL(url string){
+	resp, err := http.Get(url)
+	fmt.Println(resp.StatusCode, err)
 }
