cb16261b5685053906f41db1141f8f636f80f957
hojungpark-sfu <tacopak21@gmail.com>
2022-12-01 16:51:26 -0800

    Create list of urls

--- fastURLChecker.go
+++ fastURLChecker.go
@@ -1,5 +1,11 @@
 package main
 
 func main(){
-
+	urls := []string{
+		"https://github.com/",
+		"https://reddit.com",
+		"https://amazon.com",
+		"https://airbnb.com",
+		"https://indeed.com",
+	}
 }
